229c03da0fca97961a4d8372caad42055c3cecb7
Pedro Aranda Maciel <pedroarandamaciel2@gmail.com>
2022-08-12 23:05:43 -0300

    Se mejoro el diseño del html en general, se agregaron nuevos campos para las cards de reserva. Implementacion de JSON y STORAGE.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-//Implementar para la reserva
+class Reserva {
+    constructor(pelicula = "", cantMayor = 0, cantMenor = 0, id, precioTotal) {
+        this.Pelicula = pelicula;
+        this.Mayores = cantMayor;
+        this.Menores = cantMenor;
+        this.Id = id;
+        this.PrecioTotal = precioTotal
+    }
+}
 function suma(numero1 = 0, numero2 = 0) {
     return numero1 + numero2
+}
+function generarEntradaConsola(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal) {
+    return (`\n*********PELICULA: ${pelicula}\n*\n* ADULTOS: ${cantMayores} Mayores-------->SubTotal: $${precioReservaMayores}\n* CHICOS: ${cantMenores} Menores--------->SubTotal: $${precioReservaMenores}\n*\n*                        PRECIO FINAL: $${precioFinal}\n*********************************************`)
 }
 function calcularEntrada(cantidadDeEntradas = 1, edad = "") {
     const PRECIOMAYOR = 900
@@ -11,91 +22,72 @@ function calcularEntrada(cantidadDeEntradas = 1, edad = "") {
         return PRECIOMENOR * cantidadDeEntradas
     }
 }
-function entrada(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal) {
-    return (`\n*********PELICULA: ${pelicula}\n*\n* ADULTOS: ${cantMayores} Mayores-------->SubTotal: $${precioReservaMayores}\n* CHICOS: ${cantMenores} Menores--------->SubTotal: $${precioReservaMenores}\n*\n*                        PRECIO FINAL: $${precioFinal}\n*********************************************`)
+function insertarHtml() {
+    const divEntradas = document.getElementById("divEntradas")
+    divEntradas.innerText = ""
+    reservas.forEach(reserva => {
+        divEntradas.innerHTML += `
+        <div class="cardReserva" id="reserva${reserva.Id}">
+        <p>Pelicula: ${reserva.Pelicula}</p>
+        <p>Mayores: ${reserva.Mayores}</p>
+        <p>Menores: ${reserva.Menores}</p>
+        <p>Reserva Numero: ${reserva.Id}</p>
+        <p>Precio Total: ${reserva.PrecioTotal}</p>
+        <div>
+        <button class="btnCancelarRes" id="btnReserva${reservas.length}">Remover Reserva</button>
+        </div>
+        </div>
+        `
+    })
 }
-function cancelarReserva(idReserva) {
-    reservas.splice(idReserva - 1, 1)
-    reservas.push(`La reserva ${idReserva} fue cancelada `)
-}
-class Reserva {
-    constructor(pelicula = "", cantMayor = 0, cantMenor = 0, idReserva) {
-        this.Pelicula = pelicula;
-        this.Mayores = cantMayor;
-        this.Menores = cantMenor;
-        this.NumeroReserva = idReserva;
-    }
-}
-alert("Bienvenido al sistema de reserva de entradas")
 const reservas = []
-let repetir = true, conteoReserva = 1
-while (repetir) {
-    let pelicula, cantMayores = 0, cantMenores = 0, pregunta, precioReservaMayores = 0, precioReservaMenores = 0, precioFinal = 0
+let idReserva = 1
+let ultimasReservasStorage = []
 
-    pelicula = parseInt(prompt("SISTEMA DE RESERVA DE ENTRADAS\n Por favor escriba el numero segun corresponda\n 1) El Telefono Negro\n 2) La Casa Embrujada 2\n 3)Los Minions\n 4)Avatar, El Camino Del Agua"))
-    cantMayores = parseInt(prompt("Cuantas entradas para MAYORES desea reservar?"))
-    cantMenores = parseInt(prompt("Cuantas entradas para MENORES desea reservar?"))
-
-    if ((isNaN(cantMayores) || isNaN(cantMenores)) || (cantMayores === 0 && cantMenores === 0)) {
-        alert("Cantidad de entradas ingresada no valida, por favor intente nuevamente")
-        console.log("Cantidad no validad")
-        continue
-    }
-    switch (pelicula) {
-        case 1:
-            pelicula = "El Telefono Negro"
-            break
-        case 2:
-            pelicula = "La Casa Embrujada"
-            break
-        case 3:
-            pelicula = "Los Minions"
-            break
-        case 4:
-            pelicula = "Avatar, El Camino Del Agua"
-            break
-        default:
-            alert("Pelicula no valida, por favor intente de nuevo")
-            console.log("Ingreso de pelicula no valida")
-            continue
-    }
-    alert(`La pelicula seleccionada es ${pelicula}. Para ${cantMayores} mayores y ${cantMenores} menores`)
-    const reserva = new Reserva(pelicula, cantMayores, cantMenores, conteoReserva)
-    reservas.push(reserva)
-    conteoReserva++
-    if (cantMayores > 0) {
-        precioReservaMayores = calcularEntrada(cantMayores, "mayor")
-    }
-    if (cantMenores > 0) {
-        precioReservaMenores = calcularEntrada(cantMenores, "menor")
-    }
-    precioFinal = suma(precioReservaMayores, precioReservaMenores)
-    console.log(entrada(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal))
-    do {
-        pregunta = prompt("Gracias por su reserva! Desea realizar otra? (SI/NO)").toLowerCase()
-    } while (pregunta != "si" && pregunta != "no")
-    if (pregunta == "no") {
-        repetir = false
-        alert("Gracias por utilizar nuestros servicios, puede revisar su reserva en la consola presionando: F12>console")
-    }
+if(localStorage.getItem("reservasLS")){                               
+    ultimasReservasStorage = JSON.parse(localStorage.getItem("reservasLS"))
+    idReserva = ultimasReservasStorage.length + 1
+    ultimasReservasStorage.forEach((reserva, indice) => {
+        reservas.push(reserva)                                //igualo mi array reservas con el array de ultimasReservas(storage)
+        divEntradas.innerHTML += `
+        <div class="cardReserva" id="reserva${indice}">
+        <p>Pelicula: ${reserva.Pelicula}</p>
+        <p>Mayores: ${reserva.Mayores}</p>
+        <p>Menores: ${reserva.Menores}</p>
+        <p>Reserva Numero: ${reserva.Id}</p>
+        <p>Precio Total: ${reserva.PrecioTotal}</p>
+        <div>
+        <button class="btnCancelarRes" id="btnReserva${reservas.length}">Remover Reserva</button>
+        </div>
+        </div>
+        `
+    })
 }
-console.log(reservas)
-//cancelarReserva(1)
-//console.log(reservas)
-/*
-class Usuario {
-    constructor(nombre, apellido, email){
-        this.Nombre = nombre
-        this.Apellido = apellido
-        this.Email = email
+console.log(ultimasReservasStorage)
+const formEntrada = document.getElementById("formEntrada")
+formEntrada.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let precioReservaMayores = 0, precioReservaMenores = 0
+    let pelicula = document.getElementById("pelicula").value
+    const cantMayores = parseInt(document.getElementById("mayor").value)
+    const cantMenores = parseInt(document.getElementById("menor").value)
+    if ((cantMayores >= 0 && cantMenores >= 0) && (cantMayores != 0 || cantMenores != 0) && (pelicula != 0)) {
+        if (cantMayores > 0) {
+            precioReservaMayores = calcularEntrada(cantMayores, "mayor")
+        }
+        if (cantMenores > 0) {
+            precioReservaMenores = calcularEntrada(cantMenores, "menor")
+        }
+        let precioFinal = suma(precioReservaMayores, precioReservaMenores)
+        const entrada = new Reserva(pelicula, cantMayores, cantMenores, idReserva, precioFinal)
+        reservas.push(entrada)
+        localStorage.setItem("reservasLS", JSON.stringify(reservas))      //actualiza el array reservas a medida se generan
+        console.log(reservas)
+        console.log(generarEntradaConsola(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal))
+        idReserva++
+        insertarHtml()
+    } else {
+        console.log("Cantidad no valida")
     }
-}
-class Pelicula{
-    constructor(nombre, duracion, genero, director, actor){
-        this.Nombre = nombre
-        this.Duracion = duracion
-        this.Genero = genero
-        this.Director =director
-        this.Actor = actor
-    }
-}*/
+    formEntrada.reset()
+})
